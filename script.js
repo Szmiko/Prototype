f9ab86cb5d9f0ea6aae2aac06ab5db7fdd6a7c44
Szymon Grabowski <szmiko@wp.pl>
2018-09-25 19:00:45 +0200

    Upgrade

--- script.js
+++ script.js
@@ -21,9 +21,21 @@ Dog.prototype.wag = function() {
 	console.log("Merda ogonem!");
 };
 
+Dog.prototype.sitting = false;
+
+Dog.prototype.sit = function() {
+	if (this.sitting) {
+		console.log(this.name + " już siedzi.");
+	} else {
+		console.log(this.name + " wykonał siad.");
+		this.sitting = true;
+	};
+};
+
 var fido = new Dog("Burek", "mieszaniec", 20);
 var fluffy = new Dog("Dino", "pudel", 16);
 var spot = new Dog("Kieł", "chihuahua", 4);
+var barnaby = new Dog("Barnaba", "bokser", 18);
 
 spot.bark = function() {
 	console.log(this.name + " szczeka HAU HAU!");
@@ -40,3 +52,9 @@ fluffy.wag();
 spot.bark();
 spot.run();
 spot.wag();
+
+barnaby.sit();
+barnaby.sit();
+
+spot.sit();
+spot.sit();
